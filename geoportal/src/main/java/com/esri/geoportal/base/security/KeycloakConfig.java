958927b417aa92d9689c83e5cf0a54cf757207cc
Jeff Yutzler <jeff.yutzler@tepa.com>
2025-09-15 21:16:52 -0400

    Keycloak integration !3
    Adds authentication-keycloak.xml as an authentication option.
    Supports both federated and in-app login, depending on the presence
    or absence of keycloakAuthUrl.
    
    Also:
    - Externalizes many more of the strings used in the configuration
    XML files.
    - Allows the app to reload gracefully if a timed out or otherwise
    invalid cookie leads to a 401 when pinging GeoportalService.

---
+++ geoportal/src/main/java/com/esri/geoportal/base/security/KeycloakConfig.java
@@ -0,0 +1,53 @@
+package com.esri.geoportal.base.security;
+
+public class KeycloakConfig {
+    private String keycloakTokenUrl;
+    private String keycloakAuthUrl;
+    private String clientId;
+    private String clientSecret;
+    private String redirectUrl;
+    private String rolePrefix;
+    private String jwtSigningKey;
+    public String getKeycloakTokenUrl() {
+        return keycloakTokenUrl;
+    }
+    public void setKeycloakTokenUrl(String keycloakTokenUrl) {
+        this.keycloakTokenUrl = keycloakTokenUrl;
+    }
+    public String getKeycloakAuthUrl() {
+        return keycloakAuthUrl;
+    }
+    public void setKeycloakAuthUrl(String keycloakAuthUrl) {
+        this.keycloakAuthUrl = keycloakAuthUrl;
+    }
+    public String getClientId() {
+        return clientId;
+    }
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+    public String getClientSecret() {
+        return clientSecret;
+    }
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+    public String getRedirectUrl() {
+        return redirectUrl;
+    }
+    public void setRedirectUrl(String redirectUri) {
+        this.redirectUrl = redirectUri;
+    }
+    public String getRolePrefix() {
+        return rolePrefix;
+    }
+    public void setRolePrefix(String rolePrefix) {
+        this.rolePrefix = rolePrefix;
+    }
+    public String getJwtSigningKey() {
+        return jwtSigningKey;
+    }
+    public void setJwtSigningKey(String jwtSigningKey) {
+        this.jwtSigningKey = jwtSigningKey;
+    }
+}
