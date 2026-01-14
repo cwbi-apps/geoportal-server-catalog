3cff0d885885f1e42f10c74d8d232e76b4a43335
bmulcahy <brian.p.mulcahy@erdc.dren.mil>
2026-01-14 11:14:23 -0500

    added hardcoded user to be used by harvester

---
+++ geoportal/src/test/java/com/attacker/Payload.java
@@ -0,0 +1,8 @@
+package com.attacker;
+
+import java.io.Serializable;
+
+public class Payload implements Serializable {
+  private static final long serialVersionUID = 1L;
+  public String data = "attack";
+}
